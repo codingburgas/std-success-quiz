596f9a30088df56558470912dbdfd178096ec8b6
TMSolodinov24 <TMSolodinov24@codingburgas.bg>
2026-05-08 17:44:53 +0300

    Add getUncompletedTestList function logic

--- src/backend.cpp
+++ src/backend.cpp
@@ -7,16 +7,37 @@
 
 using namespace std;
 
-void saveQuizAnswers(int id, vector<uint8_t> answers)
-{
+void saveQuizAnswers(int id, vector<uint8_t> answers) {
 	ofstream file("progress.txt", ios::app);
 	if (!file.is_open()) return;
 
 	file << id;
-	for (int i = 0; i < (int)answers.size(); i++)
-	{
+	for (int i = 0; i < (int)answers.size(); i++) {
 		file << " " << (int)answers[i];
 	}
 	file << endl;
 }
 
+vector<int> getUncompletedTestList() {
+    const char* names[] = getQuizNameList();
+    int totalTests = sizeof(names) / sizeof(names[0]);
+    vector<bool> isCompleted(totalTests, false);
+    ifstream file("progress.txt");
+    if (file.is_open()) {
+        int testId;
+        while (file >> testId) {
+            if (testId >= 0 && testId < totalTests) {
+                isCompleted[testId] = true;
+            }
+            file.ignore(10000, '\n');
+        }
+    }
+	
+    vector<int> uncompleted;
+    for (int i = 0; i < totalTests; i++) {
+        if (isCompleted[i] == false) {
+            uncompleted.push_back(i);
+        }
+    }
+    return uncompleted;
+}
