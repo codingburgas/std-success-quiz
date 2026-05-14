35f5b89794baf57eb384b42d5b0009f2cc965ef6
SVRomanchenko24 <SVRomanchenko24@codingburgas.bg>
2026-05-14 17:02:37 +0300

    Stop cmd from showing on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <gtkmm/application.h>
 #include "../include/ui.h"
 
+#ifdef _WIN32 // WinMain
+#include <windows.h>
+#endif
+
+#ifndef _WIN32
 int main(int argc, char **argv)
+#else
+int WinMain(HINSTANCE hInstance, HINSTANCE hprevInstance, PSTR cmdline, int cmdshow) // used to hide console
+#endif
 {
 	auto app = Gtk::Application::create("std.success.quiz");
 
-	return app->make_window_and_run<mainWindow>(argc, argv);
+	return app->make_window_and_run<mainWindow>(0, 0);
 }
